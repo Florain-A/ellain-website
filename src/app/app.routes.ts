f68948b03479bda73ac118ae155274d5f9a767b5
Florain A <florainsnooki@gmail.com>
2026-07-21 16:26:22 +0530

    initial routes for modules

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { Home } from './modules/home/home';
+import { About } from './modules/about/about';
+import { Contact } from './modules/contact/contact';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: Home
+    },
+    {
+        path: 'about',
+        component: About
+    },
+    {
+        path: 'contact',
+        component: Contact
+    },
+    {
+        path: '**',
+        redirectTo: '' //nonexistent routes will redirect to home page
+    }
+];
