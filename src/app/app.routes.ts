38ee2a53d65359a6680eefe6809bdbc65cfad913
Florain A <florainsnooki@gmail.com>
2026-07-21 16:49:11 +0530

    standalone component no imports. layout component avoiding duplicate. nested routing. app lightweight.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,23 +2,32 @@ import { Routes } from '@angular/router';
 import { Home } from './modules/home/home';
 import { About } from './modules/about/about';
 import { Contact } from './modules/contact/contact';
+import { MainLayout } from './layout/main-layout/main-layout';
+
 
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: Home
-    },
-    {
-        path: 'about',
-        component: About
-    },
-    {
-        path: 'contact',
-        component: Contact
-    },
+   
     {
         path: '**',
         redirectTo: '' //nonexistent routes will redirect to home page
+    },
+    {
+        path: '',
+        component: MainLayout,
+        children: [
+            {
+                path: '',
+                component: Home
+            },
+            {
+                path: 'about',
+                component: About
+            },
+            {
+                path: 'contact',
+                component: Contact
+            }
+        ]
     }
 ];
